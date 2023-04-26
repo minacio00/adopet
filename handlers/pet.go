85cf0952039073bf735653a15a1de69abaa67acc
minacio00 <mateus_aruana@hotmail.com>
2023-04-26 10:07:11 -0300

    CRUD for pet

--- handlers/pet.go
+++ handlers/pet.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/url"
 	"regexp"
 	"strings"
@@ -64,9 +65,98 @@ func CreatePet(c *fiber.Ctx) error {
 }
 func GetAllPets(c *fiber.Ctx) error {
 	pets := &[]models.Pet{}
-	err := database.Db.Where(&pets).Error
+	err := database.Db.Find(&pets).Error
 	if err != nil {
 		println(err.Error())
 	}
 	return c.Status(200).JSON(pets)
+}
+func GetPet(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		println(err.Error())
+	}
+	pet := &models.Pet{}
+	err = database.Db.Find(&pet, "id = ?", id).Error
+	if err != nil {
+		println(err.Error())
+	}
+	if pet.ID == 0 {
+		return c.Status(404).SendString("pet não encontrado")
+	}
+	return c.Status(200).JSON(&pet)
+}
+
+func UpdatePet(c *fiber.Ctx) error {
+	c.Accepts("application/json")
+
+	body, pet := &models.Pet{}, &models.Pet{}
+	err := c.BodyParser(&body)
+	if err != nil {
+		println(err.Error())
+	}
+	// checar se existe no banco um pet com o id dado
+	if body.ID == 0 {
+		return c.Status(400).SendString("id inválido")
+	}
+	err = database.Db.Find(&pet, "id = ?", body.ID).Error
+	if err != nil {
+		println(err.Error())
+	}
+	if pet.ID == 0 {
+		return c.Status(404).SendString("pet não encontrado")
+	}
+
+	if body.AbrigoID == 0 || body.Nome == "" || body.Idade == "" || body.Imagem == "" {
+		return c.Status(400).SendString("campos não podem estar vazios")
+	}
+	regex := regexp.MustCompile(`^[a-zA-Z]+$`)
+	if !regex.MatchString(body.Nome) {
+		return c.Status(400).SendString("Campo nome com caracteres inválidos")
+	}
+
+	//checa se o abrigo existe
+	abrigo := models.Abrigo{}
+	err = database.Db.Model(&models.Abrigo{}).Find(&abrigo, "id = ?", body.AbrigoID).Error
+	if err != nil {
+		println(err.Error())
+	}
+	if abrigo.ID == 0 {
+		return c.Status(400).SendString("abrigo não encontrado")
+	}
+	if !isValidImageURL(pet.Imagem) {
+		return c.Status(400).SendString("url de imagem inválida")
+	}
+	json.Unmarshal(c.Body(), &pet)
+	err = database.Db.Save(&pet).Error
+	if err != nil {
+		println(err.Error())
+	}
+	return c.Status(200).JSON(&pet)
+
+}
+
+func DeletePet(c *fiber.Ctx) error {
+	c.Accepts("application/json")
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		println(err.Error())
+	}
+	if id == 0 {
+		return c.Status(404).SendString("pet não encontrado")
+	}
+	pet := models.Pet{}
+	err = database.Db.Find(&pet, "id = ?", id).Error
+	if err != nil {
+		println(err.Error())
+	}
+	if pet.ID == 0 {
+		return c.Status(404).SendString("pet não encontrado")
+	}
+
+	err = database.Db.Delete(&pet, "id = ?", id).Error
+	if err != nil {
+		println(err.Error())
+	}
+	return c.Status(200).SendString("pet deletado com sucesso")
 }
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	app := fiber.New()
+
 	database.Connectdb()
 
 	app.Post("/tutores", handlers.CreateTutor)
@@ -15,7 +16,11 @@ func main() {
 	app.Patch("/tutores/:id", handlers.UpatadeTutor)
 	app.Delete("/tutores/:id", handlers.DeleteTutor)
 	app.Get("/tutores/:id", handlers.FindTutor)
+
 	app.Post("/pet", handlers.CreatePet)
 	app.Get("/pets", handlers.GetAllPets)
+	app.Get("/pet/:id", handlers.GetPet)
+	app.Put("/pet", handlers.UpdatePet)
+	app.Delete("/pet/:id", handlers.DeletePet)
 	app.Listen(":8080")
 }
